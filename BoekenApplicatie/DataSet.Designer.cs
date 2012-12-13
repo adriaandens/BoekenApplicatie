9a8a91f7a69dd63e0da2cff6266273291d79429a
mrPeanut <laurensmarcelis@hotmail.com>
2012-12-14 00:14:45 +0100

    add ALL the searches
    -> zoeken op categorie gelukt
    -> zoeken op uitgever gemaakt
    -> zoeken reset gemaakt
    -> dbml opgezet -> klasse voor LINQ en DataBase samen te laten werken
    graag gedaan tibo. XD is nodig voor 5

--- BoekenApplicatie/DataSet.Designer.cs
+++ BoekenApplicatie/DataSet.Designer.cs
@@ -30,29 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.titel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categorieID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isbnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BoekenBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.boekenDataSet = new BoekenApplicatie.BoekenDataSet();
             this.boekBoekenlijstTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.BoekBoekenlijstTableAdapter();
             this.boekenlijstTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.BoekenlijstTableAdapter();
             this.categorieTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.CategorieTableAdapter();
             this.boekTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.BoekTableAdapter();
-            this.label1 = new System.Windows.Forms.Label();
             this.Zoeken = new System.Windows.Forms.Label();
             this.titelzoeken = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.isbnzoeken = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.boekenDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Cat_Search = new System.Windows.Forms.ComboBox();
             this.categorieBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.titel = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.categorieID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.isbnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.boekenDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.all = new System.Windows.Forms.Button();
+            this.Uit_Search = new System.Windows.Forms.ComboBox();
+            this.uitgeverBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.uitgeverTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.UitgeverTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BoekenBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boekenDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categorieBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uitgeverBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -64,10 +69,29 @@
             this.categorieID,
             this.isbnDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.BoekenBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 123);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(343, 192);
+            this.dataGridView1.Size = new System.Drawing.Size(342, 192);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // titel
+            // 
+            this.titel.DataPropertyName = "titel";
+            this.titel.HeaderText = "titel";
+            this.titel.Name = "titel";
+            // 
+            // categorieID
+            // 
+            this.categorieID.DataPropertyName = "categorieID";
+            this.categorieID.HeaderText = "categorieID";
+            this.categorieID.Name = "categorieID";
+            this.categorieID.ReadOnly = true;
+            // 
+            // isbnDataGridViewTextBoxColumn
+            // 
+            this.isbnDataGridViewTextBoxColumn.DataPropertyName = "isbn";
+            this.isbnDataGridViewTextBoxColumn.HeaderText = "isbn";
+            this.isbnDataGridViewTextBoxColumn.Name = "isbnDataGridViewTextBoxColumn";
             // 
             // BoekenBindingSource
             // 
@@ -97,15 +121,6 @@
             // 
             this.boekTableAdapter.ClearBeforeFill = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 277);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // Zoeken
             // 
             this.Zoeken.AutoSize = true;
@@ -121,11 +136,12 @@
             this.titelzoeken.Name = "titelzoeken";
             this.titelzoeken.Size = new System.Drawing.Size(100, 20);
             this.titelzoeken.TabIndex = 3;
+            this.titelzoeken.TextChanged += new System.EventHandler(this.titelzoeken_TextChanged_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 21);
+            this.label2.Location = new System.Drawing.Point(12, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 4;
@@ -133,7 +149,7 @@
             // 
             // isbnzoeken
             // 
-            this.isbnzoeken.Location = new System.Drawing.Point(133, 39);
+            this.isbnzoeken.Location = new System.Drawing.Point(15, 87);
             this.isbnzoeken.Name = "isbnzoeken";
             this.isbnzoeken.Size = new System.Drawing.Size(100, 20);
             this.isbnzoeken.TabIndex = 5;
@@ -142,64 +158,86 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(255, 21);
+            this.label3.Location = new System.Drawing.Point(233, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "categorie";
             // 
-            // comboBox1
+            // Cat_Search
             // 
-            this.comboBox1.DataSource = this.categorieBindingSource;
-            this.comboBox1.DisplayMember = "naam";
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(258, 39);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 7;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // boekenDataSetBindingSource
-            // 
-            this.boekenDataSetBindingSource.DataSource = this.boekenDataSet;
-            this.boekenDataSetBindingSource.Position = 0;
+            this.Cat_Search.DataSource = this.categorieBindingSource;
+            this.Cat_Search.DisplayMember = "naam";
+            this.Cat_Search.FormattingEnabled = true;
+            this.Cat_Search.Location = new System.Drawing.Point(236, 39);
+            this.Cat_Search.Name = "Cat_Search";
+            this.Cat_Search.Size = new System.Drawing.Size(121, 21);
+            this.Cat_Search.TabIndex = 7;
+            this.Cat_Search.SelectedIndexChanged += new System.EventHandler(this.Cat_Search_SelectedIndexChanged);
             // 
             // categorieBindingSource
             // 
             this.categorieBindingSource.DataMember = "Categorie";
             this.categorieBindingSource.DataSource = this.boekenDataSetBindingSource;
             // 
-            // titel
+            // boekenDataSetBindingSource
             // 
-            this.titel.DataPropertyName = "titel";
-            this.titel.HeaderText = "titel";
-            this.titel.Name = "titel";
+            this.boekenDataSetBindingSource.DataSource = this.boekenDataSet;
+            this.boekenDataSetBindingSource.Position = 0;
             // 
-            // categorieID
+            // all
             // 
-            this.categorieID.DataPropertyName = "categorieID";
-            this.categorieID.HeaderText = "categorieID";
-            this.categorieID.Name = "categorieID";
-            this.categorieID.ReadOnly = true;
+            this.all.Location = new System.Drawing.Point(144, 36);
+            this.all.Name = "all";
+            this.all.Size = new System.Drawing.Size(75, 23);
+            this.all.TabIndex = 8;
+            this.all.Text = "Show All";
+            this.all.UseVisualStyleBackColor = true;
+            this.all.Click += new System.EventHandler(this.all_Click);
             // 
-            // isbnDataGridViewTextBoxColumn
+            // Uit_Search
             // 
-            this.isbnDataGridViewTextBoxColumn.DataPropertyName = "isbn";
-            this.isbnDataGridViewTextBoxColumn.HeaderText = "isbn";
-            this.isbnDataGridViewTextBoxColumn.Name = "isbnDataGridViewTextBoxColumn";
+            this.Uit_Search.DataSource = this.uitgeverBindingSource;
+            this.Uit_Search.DisplayMember = "naam";
+            this.Uit_Search.FormattingEnabled = true;
+            this.Uit_Search.Location = new System.Drawing.Point(236, 87);
+            this.Uit_Search.Name = "Uit_Search";
+            this.Uit_Search.Size = new System.Drawing.Size(121, 21);
+            this.Uit_Search.TabIndex = 9;
+            this.Uit_Search.SelectedIndexChanged += new System.EventHandler(this.Uit_Search_SelectedIndexChanged);
+            // 
+            // uitgeverBindingSource
+            // 
+            this.uitgeverBindingSource.DataMember = "Uitgever";
+            this.uitgeverBindingSource.DataSource = this.boekenDataSet;
+            // 
+            // uitgeverTableAdapter
+            // 
+            this.uitgeverTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "uitgever";
             // 
             // DataSet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 327);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Uit_Search);
+            this.Controls.Add(this.all);
+            this.Controls.Add(this.Cat_Search);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.isbnzoeken);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.titelzoeken);
             this.Controls.Add(this.Zoeken);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "DataSet";
             this.Text = "Form1";
@@ -207,8 +245,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BoekenBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boekenDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.categorieBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uitgeverBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,18 +262,22 @@
         private BoekenDataSetTableAdapters.BoekenlijstTableAdapter boekenlijstTableAdapter;
         private BoekenDataSetTableAdapters.CategorieTableAdapter categorieTableAdapter;
         private BoekenDataSetTableAdapters.BoekTableAdapter boekTableAdapter;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label Zoeken;
         private System.Windows.Forms.TextBox titelzoeken;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox isbnzoeken;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Cat_Search;
         private System.Windows.Forms.BindingSource categorieBindingSource;
         private System.Windows.Forms.BindingSource boekenDataSetBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn titel;
         private System.Windows.Forms.DataGridViewTextBoxColumn categorieID;
         private System.Windows.Forms.DataGridViewTextBoxColumn isbnDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button all;
+        private System.Windows.Forms.ComboBox Uit_Search;
+        private System.Windows.Forms.BindingSource uitgeverBindingSource;
+        private BoekenDataSetTableAdapters.UitgeverTableAdapter uitgeverTableAdapter;
+        private System.Windows.Forms.Label label1;
 
     }
 }
--- BoekenApplicatie/DataSet.cs
+++ BoekenApplicatie/DataSet.cs
@@ -11,7 +11,7 @@ namespace BoekenApplicatie
 {
     public partial class DataSet : Form
     {
-        private BoekenDataSet ds = new BoekenDataSet();
+        private BoekenDataContext dc = new BoekenDataContext();
         public DataSet()
         {
             InitializeComponent();
@@ -25,6 +25,8 @@ namespace BoekenApplicatie
 
         private void DataSet_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'boekenDataSet.Uitgever' table. You can move, or remove it, as needed.
+            this.uitgeverTableAdapter.Fill(this.boekenDataSet.Uitgever);
             // TODO: This line of code loads data into the 'boekenDataSet.Boek' table. You can move, or remove it, as needed.
             this.boekTableAdapter.Fill(this.boekenDataSet.Boek);
             // TODO: This line of code loads data into the 'boekenDataSet.Categorie' table. You can move, or remove it, as needed.
@@ -48,15 +50,64 @@ namespace BoekenApplicatie
             BoekenBindingSource.Filter = filterTekst;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Cat_Search_SelectedIndexChanged(object sender, EventArgs e)
         {
-            String cat = comboBox1.SelectedValue.ToString().Trim();
-            var filter = from c in ds.Categorie
-                         where c.naam == cat
-                         from b in ds.Boek
-                         where b.categorieID == c.categorieID
-                         select new { b.titel, b.isbn};
-            BoekenBindingSource.DataSource = filter;
+            DataRowView cat = Cat_Search.SelectedItem as DataRowView;
+            string sValue = "";
+            if (cat != null)
+            {
+                sValue = cat.Row["naam"] as string;
+            }
+            var filter = from c in dc.Categories
+                                         where c.naam == sValue
+                                         from b in dc.Boeks
+                                         where b.categorieID == c.categorieID
+                                         select new { b.categorieID };
+            string nieuw = "";
+            foreach (var prod in filter)
+            {
+                nieuw = prod.categorieID;
+            }
+            this.Text = nieuw;
+            string filterTekst = "categorieID like '" + nieuw  + "'";
+            BoekenBindingSource.Filter = filterTekst;
+        }
+
+        private void all_Click(object sender, EventArgs e)
+        {
+            titelzoeken.Text = "";
+            isbnzoeken.Text = "";
+            BoekenBindingSource.RemoveFilter();
+
+        }
+
+        private void titelzoeken_TextChanged_1(object sender, EventArgs e)
+        {
+            string filterTekst = "titel like '%" + titelzoeken.Text.Trim() + "%'";
+            BoekenBindingSource.Filter = filterTekst;
+        }
+
+        private void Uit_Search_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRowView uit = Uit_Search.SelectedItem as DataRowView;
+            string sValue = "";
+            if (uit != null)
+            {
+                sValue = uit.Row["naam"] as string;
+            }
+            var filter = from u in dc.Uitgevers
+                         where u.naam == sValue
+                         from b in dc.Boeks
+                         where b.uitgeverID == u.uitgeverID
+                         select new { b.uitgeverID };
+            string nieuw = "";
+            foreach (var prod in filter)
+            {
+                nieuw = prod.uitgeverID;
+            }
+            this.Text = nieuw;
+            string filterTekst = "uitgeverID like '" + nieuw + "'";
+            BoekenBindingSource.Filter = filterTekst;
         }
 
 
