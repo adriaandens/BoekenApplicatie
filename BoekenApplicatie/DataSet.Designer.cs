3b6d841b3cd09a29e6dcacc88254459e43288564
mrPeanut <laurensmarcelis@hotmail.com>
2012-12-14 00:43:05 +0100

    layout changes

--- BoekenApplicatie/DataSet.Designer.cs
+++ BoekenApplicatie/DataSet.Designer.cs
@@ -29,10 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.titel = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.categorieID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.isbnDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BoekenBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.boekenDataSet = new BoekenApplicatie.BoekenDataSet();
             this.boekBoekenlijstTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.BoekBoekenlijstTableAdapter();
@@ -52,46 +50,42 @@
             this.uitgeverBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.uitgeverTableAdapter = new BoekenApplicatie.BoekenDataSetTableAdapters.UitgeverTableAdapter();
             this.label1 = new System.Windows.Forms.Label();
+            this.boekBoekenlijstBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.titel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categorieID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.isbn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BoekenBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boekenDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categorieBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.uitgeverBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boekBoekenlijstBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.titel,
             this.categorieID,
-            this.isbnDataGridViewTextBoxColumn});
+            this.isbn});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
             this.dataGridView1.DataSource = this.BoekenBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 123);
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Highlight;
+            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 132);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(342, 192);
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Size = new System.Drawing.Size(343, 207);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // titel
-            // 
-            this.titel.DataPropertyName = "titel";
-            this.titel.HeaderText = "titel";
-            this.titel.Name = "titel";
-            // 
-            // categorieID
-            // 
-            this.categorieID.DataPropertyName = "categorieID";
-            this.categorieID.HeaderText = "categorieID";
-            this.categorieID.Name = "categorieID";
-            this.categorieID.ReadOnly = true;
-            // 
-            // isbnDataGridViewTextBoxColumn
-            // 
-            this.isbnDataGridViewTextBoxColumn.DataPropertyName = "isbn";
-            this.isbnDataGridViewTextBoxColumn.HeaderText = "isbn";
-            this.isbnDataGridViewTextBoxColumn.Name = "isbnDataGridViewTextBoxColumn";
             // 
             // BoekenBindingSource
             // 
@@ -124,54 +118,66 @@
             // Zoeken
             // 
             this.Zoeken.AutoSize = true;
-            this.Zoeken.Location = new System.Drawing.Point(12, 21);
+            this.Zoeken.Location = new System.Drawing.Point(16, 23);
+            this.Zoeken.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Zoeken.Name = "Zoeken";
-            this.Zoeken.Size = new System.Drawing.Size(23, 13);
+            this.Zoeken.Size = new System.Drawing.Size(31, 14);
             this.Zoeken.TabIndex = 2;
             this.Zoeken.Text = "titel";
             // 
             // titelzoeken
             // 
-            this.titelzoeken.Location = new System.Drawing.Point(15, 39);
+            this.titelzoeken.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.titelzoeken.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.titelzoeken.Location = new System.Drawing.Point(20, 42);
+            this.titelzoeken.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.titelzoeken.Name = "titelzoeken";
-            this.titelzoeken.Size = new System.Drawing.Size(100, 20);
+            this.titelzoeken.Size = new System.Drawing.Size(133, 22);
             this.titelzoeken.TabIndex = 3;
             this.titelzoeken.TextChanged += new System.EventHandler(this.titelzoeken_TextChanged_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Location = new System.Drawing.Point(16, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(33, 14);
             this.label2.TabIndex = 4;
             this.label2.Text = "isbn";
             // 
             // isbnzoeken
             // 
-            this.isbnzoeken.Location = new System.Drawing.Point(15, 87);
+            this.isbnzoeken.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.isbnzoeken.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.isbnzoeken.Location = new System.Drawing.Point(20, 94);
+            this.isbnzoeken.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.isbnzoeken.Name = "isbnzoeken";
-            this.isbnzoeken.Size = new System.Drawing.Size(100, 20);
+            this.isbnzoeken.Size = new System.Drawing.Size(133, 22);
             this.isbnzoeken.TabIndex = 5;
             this.isbnzoeken.TextChanged += new System.EventHandler(this.isbnzoeken_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(233, 23);
+            this.label3.Location = new System.Drawing.Point(311, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.Size = new System.Drawing.Size(66, 14);
             this.label3.TabIndex = 6;
             this.label3.Text = "categorie";
             // 
             // Cat_Search
             // 
+            this.Cat_Search.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.Cat_Search.DataSource = this.categorieBindingSource;
             this.Cat_Search.DisplayMember = "naam";
+            this.Cat_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Cat_Search.FormattingEnabled = true;
-            this.Cat_Search.Location = new System.Drawing.Point(236, 39);
+            this.Cat_Search.Location = new System.Drawing.Point(315, 42);
+            this.Cat_Search.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Cat_Search.Name = "Cat_Search";
-            this.Cat_Search.Size = new System.Drawing.Size(121, 21);
+            this.Cat_Search.Size = new System.Drawing.Size(160, 22);
             this.Cat_Search.TabIndex = 7;
             this.Cat_Search.SelectedIndexChanged += new System.EventHandler(this.Cat_Search_SelectedIndexChanged);
             // 
@@ -187,22 +193,29 @@
             // 
             // all
             // 
-            this.all.Location = new System.Drawing.Point(144, 36);
+            this.all.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.all.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.all.Location = new System.Drawing.Point(184, 40);
+            this.all.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.all.Name = "all";
-            this.all.Size = new System.Drawing.Size(75, 23);
+            this.all.Size = new System.Drawing.Size(100, 25);
             this.all.TabIndex = 8;
             this.all.Text = "Show All";
-            this.all.UseVisualStyleBackColor = true;
+            this.all.UseVisualStyleBackColor = false;
             this.all.Click += new System.EventHandler(this.all_Click);
             // 
             // Uit_Search
             // 
+            this.Uit_Search.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.Uit_Search.DataSource = this.uitgeverBindingSource;
             this.Uit_Search.DisplayMember = "naam";
+            this.Uit_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Uit_Search.FormattingEnabled = true;
-            this.Uit_Search.Location = new System.Drawing.Point(236, 87);
+            this.Uit_Search.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Uit_Search.Location = new System.Drawing.Point(315, 94);
+            this.Uit_Search.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Uit_Search.Name = "Uit_Search";
-            this.Uit_Search.Size = new System.Drawing.Size(121, 21);
+            this.Uit_Search.Size = new System.Drawing.Size(160, 22);
             this.Uit_Search.TabIndex = 9;
             this.Uit_Search.SelectedIndexChanged += new System.EventHandler(this.Uit_Search_SelectedIndexChanged);
             // 
@@ -218,17 +231,43 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(236, 70);
+            this.label1.Location = new System.Drawing.Point(315, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.Size = new System.Drawing.Size(59, 14);
             this.label1.TabIndex = 10;
             this.label1.Text = "uitgever";
             // 
+            // boekBoekenlijstBindingSource
+            // 
+            this.boekBoekenlijstBindingSource.DataMember = "BoekBoekenlijst";
+            this.boekBoekenlijstBindingSource.DataSource = this.boekenDataSet;
+            // 
+            // titel
+            // 
+            this.titel.DataPropertyName = "titel";
+            this.titel.HeaderText = "titel";
+            this.titel.Name = "titel";
+            // 
+            // categorieID
+            // 
+            this.categorieID.DataPropertyName = "categorieID";
+            this.categorieID.HeaderText = "categorieID";
+            this.categorieID.Name = "categorieID";
+            this.categorieID.ReadOnly = true;
+            // 
+            // isbn
+            // 
+            this.isbn.DataPropertyName = "isbn";
+            this.isbn.HeaderText = "isbn";
+            this.isbn.Name = "isbn";
+            // 
             // DataSet
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 327);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(768, 352);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Uit_Search);
             this.Controls.Add(this.all);
@@ -239,6 +278,9 @@
             this.Controls.Add(this.titelzoeken);
             this.Controls.Add(this.Zoeken);
             this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "DataSet";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.DataSet_Load);
@@ -248,6 +290,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.categorieBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boekenDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.uitgeverBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boekBoekenlijstBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -270,14 +313,15 @@
         private System.Windows.Forms.ComboBox Cat_Search;
         private System.Windows.Forms.BindingSource categorieBindingSource;
         private System.Windows.Forms.BindingSource boekenDataSetBindingSource;
-        private System.Windows.Forms.DataGridViewTextBoxColumn titel;
-        private System.Windows.Forms.DataGridViewTextBoxColumn categorieID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn isbnDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button all;
         private System.Windows.Forms.ComboBox Uit_Search;
         private System.Windows.Forms.BindingSource uitgeverBindingSource;
         private BoekenDataSetTableAdapters.UitgeverTableAdapter uitgeverTableAdapter;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categorieID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn isbn;
+        private System.Windows.Forms.BindingSource boekBoekenlijstBindingSource;
 
     }
 }
