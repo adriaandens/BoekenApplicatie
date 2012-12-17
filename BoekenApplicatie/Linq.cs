fd4afbaec3471f8af88028cd9dbbfedc8350cdc7
tiboke1992 <tibo@hulens.be>
2012-12-17 23:55:51 +0100

    error in populatie van klaslijsten?

--- BoekenApplicatie/Linq.cs
+++ BoekenApplicatie/Linq.cs
@@ -20,12 +20,23 @@ namespace BoekenApplicatie
 
         private void Linq_Load(object sender, EventArgs e)
         {
-            var q1 = from boek in dc.Boeks
-                     group boek by boek.categorieID into gg
-                     select new { Categorie = gg.Key, aantal = gg.Count() };
-                     
+            //var q1 = from boek in dc.Boeks
+            //         join cc in dc.Categories on boek.categorieID equals cc.categorieID
+            //         group boek by boek.categorieID into gg
+            //         select new { Categorie = gg.Key, aantal = gg.Count() };
 
+            var q1 = from cats in dc.Categories
+                     join boek in dc.Boeks on cats.categorieID equals boek.categorieID
+                     group cats by cats.naam into gg
+                     orderby gg.Key
+                     select new { Categorie = gg.Key, aantal = gg.Count() };
+                    
             this.dgv1.DataSource = q1;
+
+            //query 2
+            var q2 = from b in dc.BoekBoekenlijsts
+                     select new { b.klas, b.id_boek, b.huurprijs, b.schoolprijs };
+            this.dgv2.DataSource = q2;
         }
     }
 }
